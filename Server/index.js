7fa0a412a30005a408bdaf7ef53ffb9307a178fd
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-07-07 10:47:23 +0530

    Refactored Vidly Backend

--- Server/index.js
+++ Server/index.js
@@ -1,75 +1,15 @@
 console.clear();
-require("express-async-errors");
 const winston = require("winston");
-require("winston-mongodb");
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const config = require("config");
-const error = require("./middleware/error");
-const { constant } = require("lodash");
 const app = express();
+
 app.use(cors());
 
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.name, ex);
-  setTimeout(() => {
-    process.exit(1);
-  }, 1000); // Delay of 1 second (adjust as needed)
-});
-
-process.on("unhandledRejection", (ex) => {
-  winston.error(ex.name, ex);
-  setTimeout(() => {
-    process.exit(1);
-  }, 1000); // Delay of 1 second (adjust as needed)
-});
-
-// winston.createLogger({
-//   exceptionHandlers: [
-//     new winston.transports.File({ filename: "uncaughtExceptions.log" }),
-//   ],
-//   rejectionHandlers: [
-//     new winston.transports.File({ filename: "unhandledRejections.log" }),
-//   ],
-// });
-
-// throw new Error("Uncaught Exception");
-// const p = Promise.reject(new Error("Uncaught Exception Promise"));
-
-// p.then(() => {
-//   console.log("Success");
-// }).catch(() => {
-//   console.log("Rejected ");
-// });
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey not found...");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost/Vidly")
-  .then(() => console.log("Connected to database..."))
-  .catch((err) => winston.error(err.name, err));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
---
+++ Server/startup/logging.js
@@ -0,0 +1,42 @@
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+
+module.exports = function () {
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.Console({ format: winston.format.simple() })
+  );
+
+  process.on("uncaughtException", (ex) => {
+    winston.error(ex.name, ex);
+    setTimeout(() => {
+      process.exit(1);
+    }, 1000); // Delay of 1 second (adjust as needed)
+  });
+
+  process.on("unhandledRejection", (ex) => {
+    winston.error(ex.name, ex);
+    setTimeout(() => {
+      process.exit(1);
+    }, 1000); // Delay of 1 second (adjust as needed)
+  });
+
+  // winston.createLogger({
+  //   exceptionHandlers: [
+  //     new winston.transports.File({ filename: "uncaughtExceptions.log" }),
+  //   ],
+  //   rejectionHandlers: [
+  //     new winston.transports.File({ filename: "unhandledRejections.log" }),
+  //   ],
+  // });
+
+  // throw new Error("Uncaught Exception");
+  // const p = Promise.reject(new Error("Uncaught Exception Promise"));
+
+  // p.then(() => {
+  //   console.log("Success");
+  // }).catch(() => {
+  //   console.log("Rejected ");
+  // });
+};
